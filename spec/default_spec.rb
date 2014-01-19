e37d9bad228e5dc7dad6eea46c8df44a6b8d45ca
Tim Smith <tsmith@llnw.com>
2014-01-19 13:51:59 -0800

    Update Centos 5/6 tests to 5.10 and 6.5.  Add Ubuntu 13.04

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,33 +1,53 @@
 require 'spec_helper'
 
 describe 'nrpe::default' do
-  let(:chef_run) { runner.converge 'nrpe::default' }
 
-  it 'includes the _package_install recipe' do
-    expect(chef_run).to include_recipe('nrpe::_package_install')
-  end
+  context 'standard package install' do
+    let(:chef_run) do
+      runner = ChefSpec::Runner.new
+      runner.converge 'nrpe::default'
+    end
 
-  it 'installs nagios-nrpe-server package' do
-    expect(chef_run).to install_package('nagios-nrpe-server')
-  end
+    it 'includes the _package_install recipe' do
+      expect(chef_run).to include_recipe('nrpe::_package_install')
+    end
 
-  it 'starts nrpe service' do
-    expect(chef_run).to start_service('nagios-nrpe-server')
-  end
+    it 'installs nagios-nrpe-server package' do
+      expect(chef_run).to install_package('nagios-nrpe-server')
+    end
 
-  it 'adds addresses to the allowed hosts when defined' do
-    Chef::Recipe.any_instance.stub(:search)
+    it 'starts nrpe service' do
+      expect(chef_run).to start_service('nagios-nrpe-server')
+    end
 
-    chef_run = runner(
-      :nrpe => { :allowed_hosts => %w(test.host) }
+    it 'adds addresses to the allowed hosts when defined' do
+      Chef::Recipe.any_instance.stub(:search)
+
+      chef_run = runner(
+        :nrpe => { :allowed_hosts => %w(test.host) }
       ).converge 'nrpe::default'
 
-    expect(chef_run).to render_file("#{chef_run.node['nrpe']['conf_dir']}/nrpe.cfg").with_content('allowed_hosts=127.0.0.1,test.host')
+      expect(chef_run).to render_file("#{chef_run.node['nrpe']['conf_dir']}/nrpe.cfg").with_content('allowed_hosts=127.0.0.1,test.host')
+    end
+
+    it 'does not blow up when the search returns no results' do
+      Chef::REST.any_instance.stub(:get_rest).and_return('rows' => [], 'start' => 0, 'total' => 0)
+
+      expect { chef_run }.to_not raise_error
+    end
   end
 
-  it 'does not blow up when the search returns no results' do
-    Chef::REST.any_instance.stub(:get_rest).and_return('rows' => [], 'start' => 0, 'total' => 0)
+  context 'source install' do
+    let(:chef_run) do
+      runner = ChefSpec::Runner.new
+      runner.node.set['nrpe']['install_method'] = 'source'
+      runner.converge 'nrpe::default'
+    end
 
-    expect { chef_run }.to_not raise_error
+    it 'starts nrpe service' do
+      expect(chef_run).to start_service('nrpe')
+    end
+
   end
+
 end
