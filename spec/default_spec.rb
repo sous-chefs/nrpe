0bc687b6017aa7a1e93fc10d8e9db5275a2daae8
Tim Smith <tsmith@llnw.com>
2014-01-18 17:22:04 -0800

    Add specs from the Nagios cookbooks

---
+++ spec/default_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe 'nrpe::default' do
+  let(:chef_run) { runner.converge 'nrpe::default' }
+
+  it 'includes the _package_install recipe' do
+    expect(chef_run).to include_recipe('nrpe::_package_install')
+  end
+
+  it 'installs nagios-nrpe-server package' do
+    expect(chef_run).to install_package('nagios-nrpe-server')
+  end
+
+  it 'starts nrpe service' do
+    expect(chef_run).to start_service('nagios-nrpe-server')
+  end
+
+  it 'adds addresses to the allowed hosts when defined' do
+    Chef::Recipe.any_instance.stub(:search)
+
+    chef_run = runner(
+      :nrpe => { :allowed_hosts => %w(test.host) }
+      ).converge 'nrpe::default'
+
+    expect(chef_run).to render_file("#{chef_run.node['nrpe']['conf_dir']}/nrpe.cfg").with_content('allowed_hosts=127.0.0.1,test.host')
+  end
+
+  it 'does not blow up when the search returns no results' do
+    Chef::REST.any_instance.stub(:get_rest).and_return('rows' => [], 'start' => 0, 'total' => 0)
+
+    expect { chef_run }.to_not raise_error
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+require 'chefspec'
+require 'chefspec/berkshelf'
+
+def runner(attributes = {}, environment = 'test')
+  # A workaround so that ChefSpec can work with Chef environments (from https://github.com/acrmp/chefspec/issues/54)
+  @runner ||= ChefSpec::Runner.new(:platform => 'ubuntu', :version => '12.04') do |node|
+    env = Chef::Environment.new
+    env.name environment
+    node.stub(:chef_environment).and_return env.name
+    Chef::Environment.stub(:load).and_return env
+
+    attributes.each_pair do |key, val|
+      node.set[key] = val
+    end
+  end
+end
