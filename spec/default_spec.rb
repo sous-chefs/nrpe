2764e8513bb1758dc8887e94796bdb901c77beb0
Tim Smith <tsmith@llnw.com>
2014-03-28 17:15:21 -0700

    spec to check for default allowed_hosts

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -20,4 +20,8 @@ describe 'default installation' do
     expect { chef_run }.to_not raise_error
   end
 
+  it 'expects nrpe config to allow localhost polling' do
+    expect(chef_run).to render_file('/etc/nagios/nrpe.cfg').with_content('allowed_hosts=127.0.0.1')
+  end
+
 end
