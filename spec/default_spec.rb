998523ccbd862c865e47fbccd37b92b8b0bf9b44
Tim Smith <tsmith@llnw.com>
2014-06-20 14:17:56 +0200

    Remove spec that didn't actually do anything
    Doesn’t fail even if not mocked.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -6,11 +6,6 @@ describe 'default installation' do
     runner.converge 'nrpe::default'
   end
 
-  it 'does not blow up when the search returns no results' do
-    stub_search(:role, 'monitoring').and_return([])
-    expect { chef_run }.to_not raise_error
-  end
-
   it 'expects nrpe config to allow localhost polling' do
     expect(chef_run).to render_file('/etc/nagios/nrpe.cfg').with_content('allowed_hosts=127.0.0.1')
   end
