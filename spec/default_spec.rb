a3ef035c01ab6b2d26dfb03d9e07a70f9347aa5f
Tim Smith <tsmith@llnw.com>
2014-06-20 12:58:35 +0200

    Remove commented out spec

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -6,15 +6,6 @@ describe 'default installation' do
     runner.converge 'nrpe::default'
   end
 
-  # it 'adds addresses to the allowed hosts when defined' do
-
-  #   chef_run = runner(
-  #     :nrpe => { :allowed_hosts => %w(test.host) }
-  #   ).converge 'nrpe::default'
-
-  #   expect(chef_run).to render_file("#{chef_run.node['nrpe']['conf_dir']}/nrpe.cfg").with_content('allowed_hosts=127.0.0.1,test.host')
-  # end
-
   it 'does not blow up when the search returns no results' do
     stub_search(:role, 'monitoring').and_return([])
     expect { chef_run }.to_not raise_error
