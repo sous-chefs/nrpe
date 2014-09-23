53c759d2ec4ae7aea64b05dae719b925d0b802b8
Tim Smith <tsmith@llnw.com>
2014-09-23 10:28:14 -0700

    Allow warning/critical condition in LRWP to be both Int/String

--- resources/check.rb
+++ resources/check.rb
@@ -28,8 +28,8 @@ state_attrs :command,
 # Name of the nrpe check, used for the filename and the command name
 attribute :command_name, :kind_of => String, :name_attribute => true
 
-attribute :warning_condition, :kind_of => String, :default => nil
-attribute :critical_condition, :kind_of => String, :default => nil
+attribute :warning_condition, :kind_of => [Integer, String], :default => nil
+attribute :critical_condition, :kind_of => [Integer, String], :default => nil
 attribute :command, :kind_of => String
 attribute :parameters, :kind_of => String, :default => nil
 
