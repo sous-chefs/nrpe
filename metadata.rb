3973f9ed1c51f627696d2070bce9aa4e981cea9c
Suleyman Kutlu <snk@e-snk.me>
2016-07-19 14:50:30 +0200

    Bumping up version was not needed
    reverting to 1.5.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'tsmith84@gmail.com'
 license           'Apache 2.0'
 description       'Installs and configures Nagios NRPE client'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '1.5.4'
+version           '1.5.3'
 
 recipe 'default', 'Installs and configures a nrpe client'
 %w(build-essential yum-epel).each do |cb|
