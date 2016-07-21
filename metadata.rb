0c9a6c613408a3b6b4f2384a051c5a097d4a748d
Suleyman Kutlu <snk@e-snk.me>
2016-07-21 16:04:11 +0200

    Bup up for development

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'tsmith84@gmail.com'
 license           'Apache 2.0'
 description       'Installs and configures Nagios NRPE client'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '1.5.4'
+version           '1.5.5'
 issues_url       'https://github.com/schubergphilis/nrpe/issues' if respond_to?(:issues_url)
 source_url       'https://github.com/schubergphilis/nrpe' if respond_to?(:source_url)
 
