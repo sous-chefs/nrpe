0856fda459aabcb94da6e7a049a2aa92c5572365
Dan Webb <dan.webb@damacus.io>
2026-07-02 09:32:40 +0100

    fix: start NRPE after check config

--- test/cookbooks/test/recipes/check.rb
+++ test/cookbooks/test/recipes/check.rb
@@ -2,11 +2,17 @@
 
 apt_update 'update' if platform_family?('debian')
 
-nrpe 'default'
+nrpe 'default' do
+  action %i(install configure)
+end
 
 nrpe_check 'check_root_disk_space' do
   command '/usr/lib/nagios/plugins/check_disk'
   warning_condition '5%'
   critical_condition '1%'
   parameters '-p /'
+end
+
+nrpe 'default' do
+  action :start
 end
